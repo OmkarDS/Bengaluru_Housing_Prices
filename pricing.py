275fbac6a21fd3d1182c969e8d12fc7bf4a83771
Omkar Sawant <omkarsawant5029@gmail.com>
2019-03-31 12:41:14 +0530

    Clean size column: 	Convert 'size' from object to float. 	Replace NaN with median.

--- pricing.py
+++ pricing.py
@@ -18,31 +18,45 @@ yard_pattern = re.compile('(\d*)Sq. Yards')
 meter_pattern = re.compile('(\d*)Sq. Meter')
 acre_pattern = re.compile('(\d*)Acres')
 perch_pattern = re.compile('(\d*)Perch')
-int_pattern = re.compile('(\d*)\.\d*')
+cent_pattern = re.compile('(\d*)Cents')
+guntha_pattern = re.compile('(\d*)Guntha')
+ground_pattern = re.compile('(\d*)Grounds')
+float_pattern = re.compile('(\d*)\.\d*')
 
 df['total_sqft'] = df['total_sqft'].apply(lambda row : row if re.match(yard_pattern, row)==None
-  else str(int(re.match(yard_pattern, row).group(1)) * 9))
+  else str(float(re.match(yard_pattern, row).group(1)) * 9))
 
 df['total_sqft'] = df['total_sqft'].apply(lambda row : row if re.match(meter_pattern, row)==None
-  else str(int(re.match(meter_pattern, row).group(1)) * 10.7639))
+  else str(float(re.match(meter_pattern, row).group(1)) * 10.7639))
 
 df['total_sqft'] = df['total_sqft'].apply(lambda row : row if re.match(perch_pattern, row)==None
-  else str(int(re.match(perch_pattern, row).group(1)) * 272.25))
+  else str(float(re.match(perch_pattern, row).group(1)) * 272.25))
 
 df['total_sqft'] = df['total_sqft'].apply(lambda row : row if re.match(acre_pattern, row)==None
-  else str(int(re.match(acre_pattern, row).group(1)) * 43560))
+  else str(float(re.match(acre_pattern, row).group(1)) * 43560))
+
+df['total_sqft'] = df['total_sqft'].apply(lambda row : row if re.match(cent_pattern, row)==None
+  else str(float(re.match(cent_pattern, row).group(1)) * 435.6))
+
+df['total_sqft'] = df['total_sqft'].apply(lambda row : row if re.match(guntha_pattern, row)==None
+  else str(float(re.match(guntha_pattern, row).group(1)) * 1089.08))
+
+df['total_sqft'] = df['total_sqft'].apply(lambda row : row if re.match(ground_pattern, row)==None
+  else str(float(re.match(ground_pattern, row).group(1)) * 2400))
 
 df['total_sqft'] = df['total_sqft'].apply(lambda row : row if re.match(subtract_pattern, row)== None 
-  else str((int(re.match(subtract_pattern, row).group(1)) + int(re.match(subtract_pattern, row).group(2)))//2))
+  else str((float(re.match(subtract_pattern, row).group(1)) + float(re.match(subtract_pattern, row).group(2)))//2))
 
-df['total_sqft'] = df['total_sqft'].apply(lambda row : row if re.match(int_pattern, row)==None
-  else int(re.match(int_pattern, row).group(1)))
+df['total_sqft'] = df['total_sqft'].apply(lambda row : row if re.match(float_pattern, row)==None
+  else float(re.match(float_pattern, row).group(1)))
+
+df['total_sqft'] = pd.to_numeric(df['total_sqft'])
 
 
 '''Cleaning size column'''
 #size_pattern = re.compile('\d')
-#size = df['size'].apply(lambda row : row if re.match(size_pattern, row)==None else int(re.match(size_pattern, row).group(0)))
+#size = df['size'].apply(lambda row : row if re.match(size_pattern, row)==None 
+#   else float(re.match(size_pattern, row).group(0)))
 
 df['size'] = pd.to_numeric(df['size'].str[0], errors = 'coerce')
 df['size'] = df['size'].fillna(df['size'].median())
-
